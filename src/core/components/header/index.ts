0c82ae2532b5c406896dc6b12de90eed7dace164
Samir7895 <samir@think.az>
2021-10-08 13:22:22 +0400

    Created a template for the header component, created the header itself

---
+++ src/core/components/header/index.ts
@@ -0,0 +1,13 @@
+import Component from "../../templates/components";
+
+class Header extends Component {
+    constructor(tagName: string ,className: string) {
+        super(tagName ,className);
+    }
+
+    render() {
+      return this.container
+    }
+}
+
+export default Header
