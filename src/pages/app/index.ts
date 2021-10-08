38d551754f67f4cdf615afc747f978313582cb51
Samir7895 <samir@think.az>
2021-10-08 12:07:57 +0400

    created mainPage, assigned functionality to it, and imported it into app

--- src/pages/app/index.ts
+++ src/pages/app/index.ts
@@ -1,12 +1,19 @@
+import MainPage from "../main";
+
 class App {
     private container: HTMLElement
+    private initialPage: MainPage
     constructor() {
         this.container = document.body
+        this.initialPage = new MainPage('main-page')
     }
 
     run () {
-        this.container.innerText = 'Single Page Application'
+        const mainPageHTML = this.initialPage.render()
+        this.container.append(mainPageHTML)
     }
 }
+
+//Main, Settings, Statistics
 
 export default App
