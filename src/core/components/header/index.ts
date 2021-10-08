652667755de90e4f72e6e4e17775343515e99f3e
Samir7895 <samir@think.az>
2021-10-08 13:25:59 +0400

    Created an Array for buttons

--- src/core/components/header/index.ts
+++ src/core/components/header/index.ts
@@ -1,8 +1,28 @@
 import Component from "../../templates/components";
+import {PageIds} from "../../../pages/app";
+
+const Buttons = [
+    {
+        id: PageIds.MainPage,
+        text: 'Main-page'
+    },
+    {
+        id: PageIds.SettingsPage,
+        text: 'Settings-page'
+    },
+    {
+        id: PageIds.StatisticsPage,
+        text: 'Statistic-page'
+    }
+]
 
 class Header extends Component {
     constructor(tagName: string ,className: string) {
         super(tagName ,className);
+    }
+
+    renderPageButtons() {
+      const pageButtons = document.createElement('div')
     }
 
     render() {
